d0c83412ee1ec792af0e63f04c4d0d860e29bd37
anjali bandekar <anjalipj2002@gmail.com>
2025-05-13 16:48:45 +0800

    chatbot.py added

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+# The library and API Key is added
+import google.generativeai as genai
+import time
+import gradio as gr
+
+# API key 
+GOOGLE_API_KEY = "AIzaSyD9hDOBCiyaZLl6gbmZiJQqdF032gcMBBE"
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Model configuration
+model = genai.GenerativeModel('gemini-2.0-flash')
+
+chat = model.start_chat(history=[])
+
+# Transform Gradio history to Gemini format
+def transform_history(history):
+    new_history = []
+    for chat in history:
+        new_history.append({"parts": [{"text": chat[0]}], "role": "user"})
+        new_history.append({"parts": [{"text": chat[1]}], "role": "model"})
+    return new_history
+
+def response(message, history):
+    global chat
+    # The history will be the same as in Gradio, the 'Undo' and 'Clear' buttons will work correctly.
+    chat.history = transform_history(history)
+    response = chat.send_message(message)
+    response.resolve()
+
+    # Each character of the answer is displayed
+    for i in range(len(response.text)):
+        time.sleep(0.05)
+        yield response.text[: i+1]
+
+gr.ChatInterface(response, title='Gemini Chat', textbox=gr.Textbox(placeholder="Question to Gemini")).launch(debug=True)
